631b3916f8ae69aae13625e2cf8247b9e6264721
justinbesteman <justin@besteman.io>
2016-11-02 12:29:27 -0400

    Finished for grading

--- assets/javascript/mainjs.js
+++ assets/javascript/mainjs.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 
 
 
-
+/**
+ * Object to hold time of the countdown
+ * It will append to the page the time
+ * Handle the logic if the players runs out of time
+ * @type {Object}
+ */
 var stopwatch = {
 
     time: 15,
@@ -10,66 +15,75 @@ var stopwatch = {
 
     
     reset: function () {
+
         stopwatch.time = 15;
         
-        console.log("Reset Time " , this.time);
-        //counter = 0;
-        //change the "display" div to "00:00"
           $(".info_panel").html(this.hour_glass);
-        //empty the "laps" div
 
     },
     start: function(){
-        //Use setInterval to start the count here
+  
        counter = setInterval(stopwatch.count,1000);
-       console.log("start :" , stopwatch.time);
-       console.log("Counter: " , counter)
 
     },
     stop: function(){
-        //Use clearInterval to stop the count here
+  
         clearInterval(counter);
 
     },
 
     count: function(){
-        //increment time by 1, remember we cant use "this" here
+
         stopwatch.time--;
-        //Get the current time, pass that into the stopwatch.timeConverter function, and save the result in a variable
+ 
         var hi = stopwatch.timeConverter(stopwatch.time);
-        //Use the variable you just created to show the converted time in the "display" div
+
         $(".info_panel").html(hi);
-  		//console.log("Count :" , stopwatch.time);
 
   		if (stopwatch.time == 0) {
+
   			times_up();
+
   		}
 
 
     },
     timeConverter: function(t){
-        //This function is done. You do not need to touch it. It takes the current time in seconds and converts it to minutes and seconds (mm:ss).
+     
         var minutes = Math.floor(t/60);
+
         var seconds = t - (minutes * 60);
+
         if (seconds < 10){
+
             seconds = "0" + seconds;
+
         }
+
         if (minutes === 0){
+
             minutes = "00";
+
         } else if (minutes < 10){
+
             minutes = "0" + minutes;
+
         }
 
         return minutes + ":" + seconds;
+
     }
 };
 
 
+
+/**
+ * Will pick a random question from the possible question array
+ * @return {Object of the Question}
+ */
 function question_pick (){
 
 	var turns = number_of_questions;
-
-	console.log();
 
 	if (turns > 0 ) {
 
@@ -77,39 +91,29 @@ function question_pick (){
 
 		var rand_pick = possible_questions[rand];
 
-		//console.log("Before Splice " , possible_questions);
-
 		possible_questions.splice(rand , 1);
-
-		//console.log("After Splice " , possible_questions);
-
-		console.log("Rand is " , rand);
-		console.log("rand_pick is " , rand_pick);
-
-		
-
-		console.log("Both: " , number_of_questions);
 
 		number_of_questions--;
 
-		console.log("Ather " ,number_of_questions);
-
 		return rand_pick;
-
-
 
 	} else {
 
-
 		rounds();
+
 		setTimeout(game_over_man_game_over , 2000 );
 
 	}
 
 }
 
-function generate (rand_pick) {
 
+/**
+ * Will take the question pick and append it to the page
+ * @param  {Object} rand_pick Object of the question
+ * @return {None}           Will go to the function check answer after completion
+ */
+function generate (rand_pick) {
 
 	$(".question").html(rand_pick.question);
 
@@ -122,20 +126,26 @@ function generate (rand_pick) {
 		});
 
 		guess_answer.text(rand_pick.possible[i]);
-		//guess_answer.append(question1.possible[i]);
 
 		$(".guess").append("<p>" + guess_answer.prop("outerHTML") + "</p>");
 
-
 	}
+
 	$(".question").html(rand_pick.question);
+
 	$(".info_title").html("Try to guess before the timer is done");
+
     stopwatch.start();
+
 	check_answer();
+
 }
 
 
-
+/**
+ * Will check if the user selected the right or wrong answer
+ * @return {None} Will go to the right answer or wrong answer function base of the user this
+ */
 function check_answer () {
 
 	$(".options").on("click" , function(){
@@ -143,17 +153,12 @@ function check_answer () {
 		var user_guess = $(this);
 		user_check = user_guess.data("boolean");
 
-		console.log(user_guess);
-		console.log(user_check);
-
 		if (user_check == true) {
 
-			console.log("hey");
 			$(".guess").html("");
 			right_answer();
 
 		} else {
-			console.log("bye");
 			$(".guess").html("");
 			wrong_answer();
 		}
@@ -163,128 +168,193 @@ function check_answer () {
 
 }
 
+
+/**
+ * Stops the stopwatch and html to the screen the total score
+ * @return {None} Will go to the get ready for action function
+ */
 function game_over_man_game_over(){
 
-	//$(".guess").html("Round One Done");
-
 	stopwatch.stop();
+
 	$(".info-title").html("Round Done");
 
 	$(".info_panel").html("<h2> Right Anwsers: " +  wins +"</h2>" + "<h2>  Wrong Anwser is: " +  wrongs +"</h2>");
 
 	setTimeout(get_ready_for_action, 2000);
 
-
 }
 
+
+/**
+ * Reset the variables for the second round
+ * @return {None} Will go to generate function or the rounds function base of the number if rounds played
+ */
 function get_ready_for_action(){
 
 	if (num_rounds == total_rounds){
 
 		possible_questions = [question4,question5,question6];
+
 		number_of_questions = possible_questions.length;
 
 		$(".guess").html("");
+
 		stopwatch.reset();
+
 		the_pick_is = question_pick();
+
 		generate(the_pick_is);
 
 	}
 
 	else{
 
-		$(".info_panel").html( "<h1>Final score" + "<h2> Right Anwsers: " +  wins +"</h2>" + "<h2>  Wrong Anwser is: " +  wrongs +"</h2>");
+		rounds();
 
 	}
 
 }
 
 
-
+/**
+ * Sees the number of rounds 
+ * @return {None} Will html to the screen the final score if game is done or tells the user the round number
+ */
 function rounds(){
 
-	num_rounds++;
+	if (num_rounds == 3) {
 
-	$(".info_panel").html("<h1> Round " + num_rounds + "</h1>");
+		$(".info_title").html("<h1>Game Done</h1>");
 
-	
+		$(".info_panel").html( "<h1>Final score" + "<h2> Right Anwsers: " +  wins +"</h2>" + "<h2>  Wrong Anwser is: " +  wrongs +"</h2>");
+
+	} else {
+
+		num_rounds++;
+
+		$(".info_panel").html("<h1> Round " + num_rounds + "</h1>");
+
+	}
 
 }
 
+
+/**
+ * Will get the next questions ready in between rounds
+ * @return {None} Will go the generate function
+ */
 function rinse_and_repeat(){
 
-
 	$(".guess").html("");
+
 	stopwatch.reset();
+
 	the_pick_is = question_pick();
+
 	generate(the_pick_is);
 
 
 }
 
-//$('#image-holder').html('<img src='+images[count]+ ' width="400px">');
 
+/**
+ * Handles the logic for if the user runs out of time 
+ * @return {None} Calls rinse and repeat 
+ */
 function times_up (){
 
 	$(".info-title").html("Times up the answer was " + the_pick_is.answer);
-	$(".info_panel").html("<img src=" + the_pick_is.img + " width='300px'>");
-	console.log(the_pick_is.answer);
 
-	//setTimeout($(".guess").html("") , 3000);
+	$(".info_panel").html("<img src=" + the_pick_is.img + " width='300px'>");
+
 	stopwatch.stop();
+
 	setTimeout(rinse_and_repeat , testing);
 
 }
 
 
+/**
+ * Handles logic for if the user selects the right answer
+ * @return {None} Calls rinse and repeat 
+ */
 function right_answer(){
 
 	$(".info-title").html("Good Job! It was        " + the_pick_is.answer);
-	$(".info_panel").html("<img src=" + the_pick_is.img + " width='300px'>");
-	stopwatch.stop();
-	wins++;
-	setTimeout(rinse_and_repeat , testing);
 
+	$(".info_panel").html("<img src=" + the_pick_is.img + " width='300px'>");
+
+	stopwatch.stop();
+
+	wins++;
+
+	setTimeout(rinse_and_repeat , testing);
 
 }
 
+
+/**
+ * Handles logic if the user selects the wrong answer
+ * @return {None} Calls rinse and repeat 
+ */
 function wrong_answer() {
 	
 	$(".info-title").html("Wrong! The answer was       " + the_pick_is.answer);
+
 	$(".info_panel").html("<img src=" + the_pick_is.img + " width='300px'>");
+
 	stopwatch.stop();
+
 	wrongs++;
+
 	setTimeout(rinse_and_repeat , testing);
 
 }
 
-var num_rounds = 0;
-var total_rounds = 2;
 
-var testing = 6000;
-
-var possible_questions = [question1,question2,question3];
-
-var number_of_questions = possible_questions.length;
-
-var wins = 0;
-var wrongs = 0;
+var testing = 5000;
 
 
-var the_pick_is;
+$(".reset").on("click" , function (){
 
+	location.reload();
 
-$(".start_btn").on("click" , function(){
-
-
-	rounds();
-
-	the_pick_is = question_pick();
-
-	setTimeout(generate.bind(null , the_pick_is), 2000);
-
-	
 });
 
+
+
+/**
+ * Starts the game duh
+ * @return {None} Calls rounds then generate
+ */
+function start (){
+
+
+	$(".start_btn").on("click" , function(){
+
+		wins = 0;
+
+		wrongs = 0;
+
+		possible_questions = [question1,question2,question3];
+
+		number_of_questions = possible_questions.length;
+
+		num_rounds = 0;
+		total_rounds = 2;
+
+		rounds();
+
+		the_pick_is = question_pick();
+
+		setTimeout(generate.bind(null , the_pick_is), 2000);
+
+	});
+
+}
+
+
+start();
 
 });
